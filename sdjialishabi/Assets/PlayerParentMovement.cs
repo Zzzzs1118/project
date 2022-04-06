08fe06b973fb093f6cccf01737e610730b0d908b
Zzzzs1118 <102987600+Zzzzs1118@users.noreply.github.com>
2022-04-06 15:44:01 +0800

    basic1

--- sdjialishabi/Assets/PlayerParentMovement.cs
+++ sdjialishabi/Assets/PlayerParentMovement.cs
@@ -13,12 +13,21 @@ public class PlayerParentMovement : MonoBehaviour
     private Vector2 direction;
     private bool isJump = false;
     private float velocity_Y;
+    private bool jumpattack;
 
     public Transform childTransform;
+    private GameObject[] play;
+    private GameObject[] light1;
+    private GameObject lig;
+
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
 
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        jumpattack = false;
     }
     // Update is called once per frame
     void Update()
@@ -30,6 +39,7 @@ public class PlayerParentMovement : MonoBehaviour
         {
             isJump = true;
             ReadyJump();
+            jumpattack = true;
         }
         /*Vector2 position = transform.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
@@ -60,13 +70,67 @@ public class PlayerParentMovement : MonoBehaviour
         {
             velocity_Y = 0;
             childTransform.position = transform.position;
-
+            if(jumpattack == true)
+            {
+                jumpattack = false;
+                attack();
+            }
             if (childTransform.position == transform.position)
             {
                 isJump = false;
 
             }
         }
+        
         childTransform.Translate(new Vector3(0, velocity_Y) * Time.fixedDeltaTime);
     }
+
+    public bool UmbrellaAttact(Transform attacker, Transform attacked, float angle, float radius)
+    {
+        light1 = GameObject.FindGameObjectsWithTag("Light");
+        for (int i = 0; i < light1.Length; i++)
+        {
+            if(light1[i].GetComponent<Light>().enabled == true)
+            {
+                lig = light1[i];
+            }
+        }
+
+        Vector2 deltaA = attacker.position - attacked.position;
+        Vector2 direct = attacked.position - lig.transform.position;
+
+        float tmpAngle = Mathf.Acos(Vector2.Dot(deltaA.normalized, direct.normalized)) * Mathf.Rad2Deg;
+    
+        if (tmpAngle<angle* 0.5f && deltaA.magnitude<radius)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void attack()
+    {
+
+        play = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < play.Length; i++)
+        {
+            if (play[i] != this.gameObject)
+            {
+                if (UmbrellaAttact(this.gameObject.transform, play[i].transform, 45, 2))
+                {
+                    PlayerParentMovement controller = play[i].GetComponent<PlayerParentMovement>();
+                    controller.ChangeHealth(-1);
+                }
+            }
+
+        }
+        
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
